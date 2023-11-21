1bf9c2b3e55013a71936e3de840e728f821cd42d
tanhLinhphong <pvtuananh1505@gmail.com>
2023-11-21 16:36:02 +0700

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import User from './component/User';
 import './App.css';
 import UserList from './component/UserList';
+import ShoppingList from './component/ShoppingList';
 
 function App() {
   return (
   <div>
-    <User/>
-    <UserList/>
+    {/* <User/>
+    <UserList/> */}
+    <ShoppingList/>
   </div>
   );
 }
