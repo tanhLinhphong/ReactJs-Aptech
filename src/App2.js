755acc947bd1b870a4550d92056d4b190ac7ae34
tanhLinhphong <pvtuananh1505@gmail.com>
2023-11-22 00:51:59 +0700

    first commit

---
+++ src/App2.js
@@ -0,0 +1,10 @@
+import Container from 'react-bootstrap/Container';
+import PricingPage from './component/PricingPage';
+ function App2(){
+    return(
+        <div className='app-container'>
+            <PricingPage/>
+        </div>
+    )
+ }
+ export default App2;
